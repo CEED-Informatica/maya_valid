618e28e2ba5fcb697f87de1470bd2ca3f3c01337
Alfredo Oltra <aoltra@gmail.com>
2025-12-10 15:02:39 +0000

    upd: definición de report adecuado a la versión 17

--- reports/all_by_course/model.py
+++ reports/all_by_course/model.py
@@ -39,8 +39,17 @@ class WizardReportAllByCourse(models.TransientModel):
     for course in courses:
       validations = self.env['maya_valid.validation'].search([('course_id', '=', course.id), ('state', '=', 14)], order = 'student_surname asc')
 
-      pdf = self.env.ref('maya_valid.validations_pdf_report')._render_qweb_pdf([val.id for val in validations])[0]
-      data = base64.encodestring(pdf)
+      """ pdf, _ = self.env['ir.actions.report'].with_context(
+        report_data={'course': course.abbr}
+        )._render_qweb_pdf('maya_valid.validations_pdf_report',
+        res_ids=[val.id for val in validations]
+      ) """
+    
+      pdf, _ = self.env['ir.actions.report']._render_qweb_pdf(
+          'maya_valid.validations_pdf_report',
+          res_ids=[val.id for val in validations],
+          data={ 'course': course.abbr}
+        )
    
       path_tmp = os.path.join(addons_path, 'maya_core/tmp_files/reports', now, f'Convalidaciones_cerradas_{course.abbr}.pdf')
       kdb = open(path_tmp, 'wb')
@@ -66,11 +75,3 @@ class WizardReportAllByCourse(models.TransientModel):
       'url': url
     }
 
-  """   
-        data = {}
-        data['ids'] = self.env.context.get('active_ids', [])
-        data['model'] = self.env.context.get('active_model', 'ir.ui.menu')
-        data['form'] = self.read(['date_from', 'date_to', 'journal_ids', 'target_move', 'company_id'])[0]
-        used_context = self._build_contexts(data)
-        data['form']['used_context'] = dict(used_context, lang=get_lang(self.env).code)
-        return self.with_context(discard_logo_check=True)._print_report(data) """
